e66717c6990b71fea02423b94cea5939dc766559
Fernando Flauzino <36477415+FernandoFlauzino@users.noreply.github.com>
2020-07-14 09:18:31 -0300

    Update Constants.cs
    corrigido a mensagem do vencedor X

--- CaseItauJogoDaVelha/Application/Helper/Constants.cs
+++ CaseItauJogoDaVelha/Application/Helper/Constants.cs
@@ -8,7 +8,7 @@ namespace CaseItauJogoDaVelha.Application.Helper
     public static class Constants
     {
         public const string messageWinnerO = "Venceu jogador O!!";
-        public const string messageWinnerX = "Venceu jogador O!!";
+        public const string messageWinnerX = "Venceu jogador X!!";
         public const string messageHasNotWinner = "Empate!!";
         public const string errorMessageInvalidMatrix = "Invalid Matrix - 3x3";
     }
